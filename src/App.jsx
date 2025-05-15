13b900ff296d61851b6a8f7d102799219944c2d4
Sohil Chanana <schanana2003@gmail.com>
2025-05-15 04:30:09 -0400

    Home section completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { LoadingScreen } from "./components/LoadingScreen";
 import { useState } from "react";
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
+import { Home } from "./components/sections/Home";
 import "./index.css";
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
       </div>
     </>
   );
