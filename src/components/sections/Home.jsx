90817e658f114dc465670624af970b2eff9b099e
Sohil Chanana <schanana2003@gmail.com>
2025-05-16 04:33:38 -0400

    Minor changes

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,5 +1,7 @@
 import { RevealOnScroll } from "./RevealOnScroll";
 import profilePic from "../../assets/sohil_pic.jpg";
+import instaLogo from "../../assets/instagram.svg";
+import linkedinLogo from "../../assets/linkedin.svg";
 export const Home = () => {
   return (
     <section
@@ -24,7 +26,7 @@ export const Home = () => {
             building user-focused software and expanding my skills in Web
             Development, Computer Networks, and Full-Stack Development.
           </p>
-          <div className="flex justify-center space-x-4">
+          <div className="flex justify-center items-center space-x-4">
             <a
               href="#projects"
               className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden
@@ -34,10 +36,26 @@ export const Home = () => {
             </a>
             <a
               href="#contact"
-              className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-100
+              className="border-2 border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-100
               hover:translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10"
             >
               Contact Me
+            </a>
+            <a
+              href="https://www.linkedin.com/in/sohil-chanana/"
+              target="_blank"
+              className="inline-flex h-12 w-12 items-center justify-center rounded-full border-2 border-blue-500/50 transition-all duration-100 hover:bg-blue-500/10 hover:translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)]"
+              rel="noopener noreferrer"
+            >
+              <img src={linkedinLogo} alt="Linkedin" className="w-6 h-6" />
+            </a>
+            <a
+              href="https://www.instagram.com/sohil.banana/"
+              target="_blank"
+              className="inline-flex h-12 w-12 items-center justify-center rounded-full border-2 border-blue-500/50 transition-all duration-100 hover:bg-blue-500/10 hover:translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)]"
+              rel="noopener noreferrer"
+            >
+              <img src={instaLogo} alt="instagram" className="w-6 h-6" />
             </a>
           </div>
         </div>
